c2071dd53f04c58249d29cb71810517d0d015ae6
agamlatiff <agam.latiff@gmail.com>
2026-01-07 06:58:23 +0700

    Add(Sign-in): make api endpoint sign in 1

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,4 +1,4 @@
-import type { signUpValues } from "../utils/schema/user";
+import type { signInValues, signUpValues } from "../utils/schema/user";
 import * as userRepositories from "../repositories/userRepositories"
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
@@ -15,6 +15,32 @@ export const signUp = async (data: signUpValues, file: Express.Multer.File) => {
     ...data,
     password: await bcrypt.hash(data.password, 12)
   }, file.filename)
+
+  const token = jwt.sign({ id: user.id }, process.env.SECRET_AUTH ?? "", {
+    expiresIn: "1 days"
+  })
+
+  return {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    photo: user.photo_url,
+    token,
+  }
+}
+
+export const signIn = async (data: signInValues) => {
+  const isEmailExist = await userRepositories.isEmailExist(data.email)
+
+  if (isEmailExist === 0) {
+    throw new Error("Email already taken")
+  }
+
+  const user = await userRepositories.findUserByEmail(data.email);
+
+  if (!bcrypt.compareSync(data.password, user.password)) {
+    throw new Error("Email & password not match")
+  }
 
   const token = jwt.sign({ id: user.id }, process.env.SECRET_AUTH ?? "", {
     expiresIn: "1 days"
