e05037c16bdf3f369b90962a54c556c58b2fc746
agamlatiff <agam.latiff@gmail.com>
2026-01-09 03:27:24 +0700

    Feat(group): make schema validation and repository

---
+++ src/repositories/groupRepositories.ts
@@ -0,0 +1,32 @@
+import prisma from "../utils/prisma";
+import type { GroupFreeValues } from "../utils/schema/group";
+import * as userRepositories from "./userRepositories"
+
+
+export const createGroup = async (data: GroupFreeValues, photo: string, userId: string) => {
+  const owner = await userRepositories.findRole("OWNER");
+
+  return await prisma.group.create({
+    data: {
+      photo: photo,
+      name: data.name,
+      about: data.about,
+      price: 0,
+      type: "FREE",
+      room: {
+        create: {
+          created_by :userId,
+          name: data.name,
+          members: {
+            create: {
+              user_id: userId,
+              role_id: owner.id,
+            }
+          },
+          is_group: true,
+          
+        }
+      }
+    }
+  })
+}
