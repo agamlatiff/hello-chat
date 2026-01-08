3cadaa28ef0d9f535c305f7b6d7e76d43c3c4333
agamlatiff <agam.latiff@gmail.com>
2026-01-09 05:32:12 +0700

    feat(group paid): schema, repository and service file for paid group

--- src/repositories/groupRepositories.ts
+++ src/repositories/groupRepositories.ts
@@ -1,5 +1,5 @@
 import prisma from "../utils/prisma";
-import type { GroupFreeValues } from "../utils/schema/group";
+import type { GroupFreeValues, GroupPaidValues } from "../utils/schema/group";
 import * as userRepositories from "./userRepositories"
 
 
@@ -15,7 +15,7 @@ export const createFreeGroup = async (data: GroupFreeValues, photo: string, user
       type: "FREE",
       room: {
         create: {
-          created_by :userId,
+          created_by: userId,
           name: data.name,
           members: {
             create: {
@@ -24,9 +24,51 @@ export const createFreeGroup = async (data: GroupFreeValues, photo: string, user
             }
           },
           is_group: true,
-          
+
         }
       }
     }
   })
+}
+
+export const createPaidGroup = async (data: GroupPaidValues, photo: string, userId: string, assets?: string[]) => {
+  const owner = await userRepositories.findRole("OWNER");
+
+  const group = await prisma.group.create({
+    data: {
+      photo: photo,
+      name: data.name,
+      about: data.about,
+      price: Number.parseInt(data.price),
+      benefit: data.benefit,
+      type: "PAID",
+      room: {
+        create: {
+          created_by: userId,
+          name: data.name,
+          members: {
+            create: {
+              user_id: userId,
+              role_id: owner.id,
+            }
+          },
+          is_group: true,
+
+        }
+      }
+    }
+  })
+
+  if (assets) {
+    for (const asset of assets) {
+      await prisma.groupAsset.create({
+        data: {
+          filename: asset,
+          group_id: group.id,
+        }
+      })
+    }
+  }
+
+  return group
 }
--- src/services/groupService.ts
+++ src/services/groupService.ts
@@ -1,8 +1,12 @@
-import type { GroupFreeValues } from "../utils/schema/group";
+import type { GroupFreeValues, GroupPaidValues } from "../utils/schema/group";
 import * as groupRepositories from "../repositories/groupRepositories"
 
 export const createFreeGroup = async(data: GroupFreeValues, photo: string, userId: string) => {
   const group = await groupRepositories.createFreeGroup(data, photo, userId);
   
   return group;
+}
+
+export const createPaidGroup = async(data:GroupPaidValues, photo: string, userId: string, assets?: string[]) => {
+  const group = await groupRepositories.createPaidGroup(data,photo, userId, assets) 
 }
