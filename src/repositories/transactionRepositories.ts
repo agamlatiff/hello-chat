94be372af570a5df089832b2000658fdddad389d
agamlatiff <agam.latiff@gmail.com>
2026-01-11 07:22:47 +0700

    feat(transaction): add transaction flow and inntgrate midtrans payment gateway

--- src/repositories/transactionRepositories.ts
+++ src/repositories/transactionRepositories.ts
@@ -1,8 +1,19 @@
-import type { Prisma } from "@prisma/client";
+import type { Prisma, TransactionType } from "@prisma/client";
 import prisma from "../utils/prisma";
 
 export const createTransaction = async (data: Prisma.TransactionCreateInput) => {
   return await prisma.transaction.create({
     data
+  })
+}
+
+export const updateTransaction = async (id: string, type: TransactionType) => {
+  return await prisma.transaction.update({
+    where: {
+      id
+    },
+    data: {
+      type
+    }
   })
 }
