552c13b916db7f679c656249573e56e16f33f72e
agamlatiff <agam.latiff@gmail.com>
2026-02-02 12:57:43 +0700

    feat(sign up): sign up schema

--- front-end/src/features/auth/api/signUp.ts
+++ front-end/src/features/auth/api/signUp.ts
@@ -1,5 +1,19 @@
+import type { BaseResponse } from "../../../shared/types/response";
 import instanceApi from "../../../shared/utils/axios";
+import { z } from "zod"
 
-export const signUp = (data: FormData) => instanceApi.post("auth/sign-up", data, {headers: {
-  "Content-Type": "multipart/form-data"
-}}).then(res => res.data)
+export const signUpResponseSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  email: z.string(),
+  photo: z.string(),
+  token: z.string()
+})
+
+export type SignUpResponse = z.infer<typeof signUpResponseSchema>
+
+export const signUp = (data: FormData): Promise<BaseResponse<SignUpResponse>> => instanceApi.post("auth/sign-up", data, {
+  headers: {
+    "Content-Type": "multipart/form-data"
+  }
+}).then(res => res.data)
