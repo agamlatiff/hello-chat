56de576ce387689c53c5bc6ae952a5ce72499612
agamlatiff <agam.latiff@gmail.com>
2026-02-02 12:46:06 +0700

    feat(sign up): setup zod and react hook form

---
+++ front-end/src/features/auth/utils/schema.ts
@@ -0,0 +1,9 @@
+import z from "zod";
+
+export const signUpSchema = z.object({
+  avatar: z.any().refine((file: File) => file?.type?.startsWith("image/"), {message: "File must be an image"}),
+  email: z.string().email(),
+  password: z.string().min(8)
+})
+
+export type SignUpValues = z.infer<typeof signUpSchema> 
