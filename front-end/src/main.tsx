8091f04834a6a2943e4135b5d242ac3b3a07b43c
agamlatiff <agam.latiff@gmail.com>
2026-02-02 12:20:17 +0700

    feat(landing page & sign up): slicing and features based architecture

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { RouterProvider } from 'react-router'
+import router from './router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Hello World</h1>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 )
---
+++ front-end/src/router.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router";
+import LandingPage from "./features/landing/pages/LandingPage";
+import SignUpPage from "./features/auth/pages/SignUpPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />
+  },
+  {
+    path: "/sign-up",
+    element: <SignUpPage />
+  }
+])
+
+export default router
